7d81dda6e5548555536c079c88172058cf91287b
root <root@cbtest1.netronome.com>
2016-02-19 18:05:17 +0000

    [nfpshm] Recycling buffers to the pktgencap server, seems to stall after 4592 packets

--- pcap-nfpshm.c
+++ pcap-nfpshm.c
@@ -4,6 +4,9 @@
  */
 
 /** Make capturetest
+return processed;
+root@cbtest3:~# tcpreplay -i p2p1 -l 300 -K -q -p 10000 pablo/pcaps/csum.pcap
+
 ./configure  -with-nfpshm=/root/gavin/nfp-common/host
 make
 cc -fpic -I.  -I/root/gavin/nfp-common/host/src -I/usr/include/dbus-1.0 -I/usr/lib/x86_64-linux-gnu/dbus-1.0/include   -DHAVE_CONFIG_H  -D_U_="__attribute__((unused))" -g -O2    -I. -L. -o capturetest ./tests/capturetest.c libpcap.a -ldbus-1 ../nfp-common/host/build/nfp_ipc.o ../nfp-common/host/build/nfp_support.o -L/opt/netronome/lib -L/usr/lib/x86_64-linux-gnu/ -lnfp -lnfp_nffw -lhugetlbfs -ljansson
@@ -36,66 +39,8 @@ struct rtentry;		/* declarations in <net/if.h> */
 #include "nfp_ipc.h"
 #include "nfp_support.h"
 #include "pktgencap.h"
-
+#include "firmware/pcap.h"
 #include "pcap-nfpshm.h"
-
-/** struct pcap_pkt_buf_desc
- *
- * Packet buffer descriptor stored in the host and MU buffer.  The offset
- * is the 64B block offset from mu_base_s8.  num_blocks is the number
- * of 64B block spaces used in the MU buffer for the packet. The
- * sequence number is a 16/32-bit sequence number of the packet, as
- * supplied by the NBI Rx.
- *
- */
-#define PCAP_BUF_TOTAL_PKTS 1024
-#define PCAP_BUF_MAX_PKT (PCAP_BUF_TOTAL_PKTS-4)
-#ifdef __NFCC_VERSION
-struct pcap_pkt_buf_desc {
-    uint32_t offset:16;
-    uint32_t num_blocks:16;
-    uint32_t seq;
-};
-#else
-struct pcap_pkt_buf_desc {
-    uint32_t num_blocks:16;
-    uint32_t offset:16;
-    uint32_t seq;
-};
-#endif
-
-/** struct pcap_buf_hdr
- *
- *  Structure placed at start of an MU/host buffer
- *
- *  Not actually transferred to host, so really only in the MU side
- */
-struct pcap_buf_hdr {
-    uint32_t buf_seq;         /* MU/host buffer sequence number */
-    uint32_t total_packets;   /* Valid when buffer is complete */
-    uint32_t pcie_base_low;   /* Filled at pre-allocation */
-    uint32_t pcie_base_high;  /* Filled at pre-allocation */
-};
-
-/** struct pcap_buffer
- *
- * MU/host buffer layout, up to the packet data, which is placed at
- * PCAP_BUF_FIRST_PKT_OFFSET
- *
- * Note that this must be less than PCAP_BUF_FIRST_PKT_OFFSET in size
- * Note also that the pkt_add_mu_buf_desc clears this structure in a
- * 'knowledgeable manner', i.e. it knows the structure and offsets
- * intimately. So changing this structure requires changing that
- * function.
- */
-struct pcap_buffer {
-    struct pcap_buf_hdr hdr;
-    int      dmas_completed;  /* For DMA Master/slaves */
-    uint32_t pad[11];         /* Pad to 64B alignment */
-    uint32_t pkt_bitmask[PCAP_BUF_TOTAL_PKTS/32]; /* n*64B to pad
-                                                 * properly */
-    struct pcap_pkt_buf_desc pkt_desc[PCAP_BUF_MAX_PKT];
-};
 
 /** struct pcap_nfmshm - private data for NFP SHM capture firmware
  */
@@ -115,7 +60,8 @@ struct pcap_nfpshm {
     int nfp_ipc_client;
     struct pktgen_ipc_msg *pktgen_msg;
     struct nfp_ipc_msg *msg;
-                                               
+
+    int ifup;
     int msg_sent;
     int next_seq;
     int current_buffer;
@@ -167,7 +113,7 @@ nfpshm_alloc_shm(struct pcap_nfpshm *pd)
 
     pd->shm.base = nfp_shm_data(pd->nfp);
     pd->nfp_ipc = (struct nfp_ipc *)pd->shm.base;
-    fprintf(stderr,"%s %p %lld\n",__func__,pd->shm.base,pd->shm.size);
+    fprintf(stderr,"%s %p %ld\n",__func__,pd->shm.base,pd->shm.size);
     return 0;
 }
 
@@ -328,34 +274,105 @@ nfpshm_read(pcap_t *p, int cnt, pcap_handler callback, u_char *user)
     uint64_t phys_offset;
     struct pcap_buffer *pcap_buffer;
     int j;
-		struct pcap_pkthdr	pcap_header;
+
+    struct pcap_pkthdr	pcap_header;
+
+    if (!pd->ifup)
+        return -1;
+    if (p->break_loop) {
+        p->break_loop = 0;
+        return -2;
+    }
+    if (pd->current_buffer<0) {
+        if (!pd->msg_sent) {
+            fprintf(stderr, "sending message\n");
+            pd->pktgen_msg->reason = PKTGEN_IPC_RETURN_BUFFERS;
+            pd->pktgen_msg->ack = 0;
+            pd->pktgen_msg->return_buffers.buffers[0] = pd->buffers_to_recycle[0];
+            pd->pktgen_msg->return_buffers.buffers[1] = pd->buffers_to_recycle[1];
+            pd->buffers_to_recycle[0] = -1;
+            pd->buffers_to_recycle[1] = -1;
+            pd->pktgen_msg->return_buffers.buffers_to_claim = 1;
+            nfp_ipc_client_send_msg(pd->nfp_ipc, pd->nfp_ipc_client, pd->msg);
+            pd->msg_sent = 1;
+        }
+    }
+    if (pd->msg_sent) {
+        int timeout;
+        int poll;
+        struct nfp_ipc_event event;
+       
+        timeout = 1000*1000; // in usecs
+        fprintf(stderr, "polling message\n");
+        poll = nfp_ipc_client_poll(pd->nfp_ipc, pd->nfp_ipc_client, timeout, &event);
+        fprintf(stderr, "received %d\n", poll);
+        if (poll==NFP_IPC_EVENT_SHUTDOWN) {
+            pd->ifup = 0;
+            return -1;
+        }
+        if (poll==NFP_IPC_EVENT_MESSAGE) {
+            pd->msg_sent = 0;
+            struct pktgen_ipc_msg *msg;
+            msg = (struct pktgen_ipc_msg *)&event.msg->data[0];
+            if (msg->reason == PKTGEN_IPC_RETURN_BUFFERS) {
+                int claimed_buffer;
+                claimed_buffer = msg->return_buffers.buffers[0];
+                fprintf(stderr, "got claimed_buffer %d\n", claimed_buffer);
+                if (claimed_buffer>=0)
+                    pd->current_buffer = claimed_buffer;
+            }
+        }
+    }
+    if (pd->current_buffer<0) {
+        return 0;
+    }
 #define PCIE_HUGEPAGE_SIZE (1<<20)
     phys_offset = PCIE_HUGEPAGE_SIZE + (pd->current_buffer<<18);
     pcap_buffer = (struct pcap_buffer *)(pd->shm.base + phys_offset);
-    j = pd->next_pkt;
-    if (pcap_buffer->pkt_desc[j].offset==0)
-        return 0;
-    pd->next_pkt++;
-    fprintf(stderr, "%d: %04x %04x %08x\n",j,
-           pcap_buffer->pkt_desc[j].offset,
-           pcap_buffer->pkt_desc[j].num_blocks,
-           pcap_buffer->pkt_desc[j].seq
-        );
-    //mem_dump(((char *)pcap_buffer) + (pcap_buffer->pkt_desc[j].offset<<6), 64);
-    // skipping bpf
-    /* convert between timestamp formats */
-    pcap_header.ts.tv_sec = 0;
-    pcap_header.ts.tv_usec = pcap_buffer->pkt_desc[j].seq;
-    //register unsigned long long ts;
-    //ts = header->ts;
-    //if (IS_BIGENDIAN()) ts=SWAPLL(ts);
+    while ((processed<cnt) && (!p->break_loop)) {
+        j = pd->next_pkt;
+        if ((pcap_buffer->hdr.total_packets!=0) && (j == pcap_buffer->hdr.total_packets)) {
+            pd->buffers_to_recycle[0] = pd->current_buffer;
+            pd->current_buffer = -1;
+            pd->next_pkt = 0;
+            return processed;
+        }
+        if (pcap_buffer->pkt_desc[j].offset==0) {
+            usleep(10000);
+        }
+        if (pcap_buffer->pkt_desc[j].offset==0) {
+            break;
+        }
+        pd->next_pkt++;
+        fprintf(stderr, "%d: %d.%d: %04x %04x %08x\n",
+                j,
+                pcap_buffer->hdr.total_packets,
+                pcap_buffer->dmas_completed,
+                pcap_buffer->pkt_desc[j].offset,
+                pcap_buffer->pkt_desc[j].num_blocks,
+                pcap_buffer->pkt_desc[j].seq
+            );
+        //mem_dump(((char *)pcap_buffer) + (pcap_buffer->pkt_desc[j].offset<<6), 64);
+        // skipping bpf
+        /* convert between timestamp formats */
+        pcap_header.ts.tv_sec = 0;
+        pcap_header.ts.tv_usec = pcap_buffer->pkt_desc[j].seq;
+        //register unsigned long long ts;
+        //ts = header->ts;
+        //if (IS_BIGENDIAN()) ts=SWAPLL(ts);
 
-    /* Fill in our own header data */
-    pcap_header.caplen = pcap_buffer->pkt_desc[j].num_blocks*64;
-    pcap_header.len = pcap_header.caplen;
-    pd->stat.ps_recv++;
-    callback(user, &pcap_header, ((char *)pcap_buffer) + (pcap_buffer->pkt_desc[j].offset<<6));
-    return 1;
+        /* Fill in our own header data */
+        pcap_header.caplen = pcap_buffer->pkt_desc[j].num_blocks*64;
+        pcap_header.len = pcap_header.caplen;
+        pd->stat.ps_recv++;
+        callback(user, &pcap_header, ((char *)pcap_buffer) + (pcap_buffer->pkt_desc[j].offset<<6));
+        processed++;
+    }
+    if (p->break_loop) {
+        p->break_loop = 0;
+        return -2;
+    }
+    return processed;
 
 #if 0
 	unsigned int nonblocking = pd->nonblock;
@@ -603,6 +620,7 @@ nfpshm_activate(pcap_t *p)
 	int n;
     struct nfp_ipc_client_desc nfp_ipc_client_desc;
 
+    pd->ifup = 0;
     fprintf(stderr,"%s\n",__func__);
     if (nfpshm_alloc_shm(pd)<0) {
         fprintf(stderr, "Failed to allocate NFP SHM - is server running?\n");
@@ -619,9 +637,12 @@ nfpshm_activate(pcap_t *p)
     nfpshm_add(pd);
 
     pd->msg_sent = 0;
-    pd->current_buffer = 0;
+    pd->ifup = 1;
+    pd->current_buffer = -1;
     pd->next_buffer = -1;
     pd->next_pkt = 0;
+    pd->buffers_to_recycle[0] = -1;
+    pd->buffers_to_recycle[1] = -1;
     pd->msg = nfp_ipc_alloc_msg(pd->nfp_ipc, sizeof(struct pktgen_ipc_msg));
     pd->pktgen_msg = (struct pktgen_ipc_msg *)(&pd->msg->data[0]);
 
